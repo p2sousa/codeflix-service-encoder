0b425b2492fae84e57d4afcd66d67a3f7db5954a
Pablo Sousa <pablosousa.ads@gmail.com>
2020-02-25 18:56:58 -0300

    wip downalod de video do gcp

--- encoder/domain/Video.go
+++ encoder/domain/Video.go
@@ -1,6 +1,14 @@
 package domain
 
-import "encoding/json"
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"os"
+
+	"cloud.google.com/go/storage"
+	log "github.com/sirupsen/logrus"
+)
 
 type Video struct {
 	Uuid string `json:"uuid"`
@@ -15,4 +23,53 @@ func (video *Video) Unmarshal(payload []byte) Video  {
 	}
 
 	return *video
+}
+
+func (video *Video) Download(bucket string, path string) (Video, error)  {
+	ctx := context.Background()
+
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Error(err.Error())
+		video.Status = "error"
+		return *video, err
+	}
+
+	bkt := client.Bucket(bucket)
+	obj := bkt.Object(video.Path)
+	r, err := obj.NewReader(ctx)
+	if err != nil {
+		log.Error(err.Error())
+		video.Status = "error"
+		return *video, err
+	}
+
+	defer r.Close()
+
+	body, err := ioutil.ReadAll(r)
+	if err != nil {
+		log.Error(err.Error())
+		video.Status = "error"
+		return *video, err
+	}
+
+	f, err := os.Create(path + "/" + video.Uuid + "mp4")
+	if err != nil {
+		log.Error(err.Error())
+		video.Status = "error"
+		return *video, err
+	}
+
+	_, err := f.Write(body)
+	if err != nil {
+		log.Error(err.Error())
+		video.Status = "error"
+		return *video, err
+	}
+
+	defer f.Close()
+
+	log.Info("Video: " + video.Uuid + "has been storage")
+
+	return *video, nil
 }
